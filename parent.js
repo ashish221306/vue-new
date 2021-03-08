37bbf20e84a43dec0c9605cbf6ad4adedb64b42d
ashish221306 <ashishbhu221306@gmail.com>
2021-03-08 18:35:00 +0530

    ,nhxcjkgvh

--- parent.js
+++ parent.js
@@ -17,7 +17,6 @@ const parent = {
        <div class="child">
        <child  v-on:senddata="senddata($event)"/>
        </div>
-
        <h3>parent</h3>
        <input type="text" readonly placeholder="receive child data" :value="cd"/>
 
